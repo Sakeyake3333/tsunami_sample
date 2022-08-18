53aa585e11677ad3a548310091f6926bde8c5d9f
Sakeyake <109841157+Sakeyake3333@users.noreply.github.com>
2022-08-18 16:44:22 +0900

    Add files via upload

---
+++ tsunami_sample/tsunami_sample/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace tsunami_sample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.watch = new System.Windows.Forms.RichTextBox();
+            this.warning = new System.Windows.Forms.RichTextBox();
+            this.majorwarning = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // watch
+            // 
+            this.watch.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.watch.Location = new System.Drawing.Point(20, 39);
+            this.watch.Name = "watch";
+            this.watch.Size = new System.Drawing.Size(197, 358);
+            this.watch.TabIndex = 0;
+            this.watch.Text = "";
+            // 
+            // warning
+            // 
+            this.warning.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.warning.Location = new System.Drawing.Point(241, 39);
+            this.warning.Name = "warning";
+            this.warning.Size = new System.Drawing.Size(197, 358);
+            this.warning.TabIndex = 1;
+            this.warning.Text = "";
+            // 
+            // majorwarning
+            // 
+            this.majorwarning.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.majorwarning.Location = new System.Drawing.Point(465, 39);
+            this.majorwarning.Name = "majorwarning";
+            this.majorwarning.Size = new System.Drawing.Size(197, 358);
+            this.majorwarning.TabIndex = 2;
+            this.majorwarning.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(37, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 27);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "津波注意報";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Red;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(277, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 27);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "津波警報";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Fuchsia;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(487, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 27);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "大津波警報";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 487);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.majorwarning);
+            this.Controls.Add(this.warning);
+            this.Controls.Add(this.watch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox watch;
+        private System.Windows.Forms.RichTextBox warning;
+        private System.Windows.Forms.RichTextBox majorwarning;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ tsunami_sample/tsunami_sample/Form1.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tsunami_sample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private readonly HttpClient client = new HttpClient();
+        string p2p = "https://api.p2pquake.net/v2/jma/tsunami?limit=1&offset=5&order=-1";
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            var json = await client.GetStringAsync(p2p);
+            var root = JsonConvert.DeserializeObject<tsunami.Root>(json.Trim('[', ']'));
+
+            string w_1 = "", w_2 = "", w_3 = "";
+            var w_list = new Dictionary<string, string>()
+            {
+             {"MajorWarning","大津波警報" },
+             {"Warning","津波警報" },
+             {"Watch","津波注意報" },
+             {"Unknown","不明" }
+            };
+
+            foreach (var area in root.areas)
+            {
+                switch (w_list[area.grade])
+                {
+                    case "大津波警報":
+                        w_3 += area.name + "\n";
+                        break;
+                    case "津波警報":
+                        w_2 += area.name + "\n";
+                        break;
+                    case "津波注意報":
+                        w_1 += area.name + "\n";
+                        break;
+                }
+            }
+            if (w_3 != "") majorwarning.Text += w_3;
+            if (w_2 != "") warning.Text += w_2;
+            if (w_1 != "") watch.Text += w_1;
+        }
+    }
+}
